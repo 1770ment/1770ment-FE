38db657c222e73332e949d9b099b05d1816f41dc
HANTAEDONG <htd0913@gmail.com>
2025-09-15 12:59:41 +0900

    refactor: SafeAreaView를 react-native-safe-area-context에서 가져오도록 수정

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, StyleSheet, SafeAreaView, StatusBar } from 'react-native';
+import { View, StyleSheet, StatusBar } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { Header, BottomNavigation } from '../../widgets';
 import { useNavigation } from '../../features';
 import { HomeScreen } from './HomeScreen';
